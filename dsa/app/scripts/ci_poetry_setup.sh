3cd92ba813a742ef62a64c5ad39174024b34baca
Amrit Prusty <amrit.prusty@lowes.com>
2025-11-16 16:04:48 +0530

    mod lc

--- dsa/app/scripts/ci_poetry_setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -eo pipefail
-# Needed so the shell can find the poetry binary that is install
-export PATH=$PATH:/home/carbon/.local/bin
-
-# Installs poetry
-pip install -U poetry
-# Install all deps
-poetry install
