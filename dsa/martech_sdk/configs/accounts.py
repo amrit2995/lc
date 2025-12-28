0cc75f6f3a399464bacd8bb354118a0a7850c183
Amrit Prusty <amrit.prusty@lowes.com>
2025-12-29 00:33:10 +0530

    tex

--- dsa/martech_sdk/configs/accounts.py
+++
@@ -1,9 +0,0 @@
-from martech_sdk.configs.mixins import ConfigMixins
-
-class LMNAccount(ConfigMixins):
-    ACCOUNT_TYPE = "lmn"
-    PINTEREST_ID = "549764189253"
-
-class EnterpriseAccount(ConfigMixins):
-    ACCOUNT_TYPE = "enterprise"
-    PINTEREST_ID = "549755856460"
